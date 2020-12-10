69945e99952b592d0df811a486ab244379b0c939
lightyears1998 <lightyears1998@hotmail.com>
2020-12-10 21:24:25 +0800

    migrate
    从 ledger-moment 以及 day-primer-desktop 项目迁移代码

---
+++ pages/_app.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import type { AppProps } from "next/app";
+
+import { theme } from "../lib/theme";
+
+export default function MyApp(props: AppProps): JSX.Element {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <title>Day Primer Desktop</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from "react";
+import Head from "next/head";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { DataGrid } from "@material-ui/data-grid";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      textAlign: "center", paddingTop: theme.spacing(4), paddingBottom: theme.spacing(4)
+    }
+  })
+);
+
+const HomePage = (): JSX.Element => {
+  const classes = useStyles({});
+
+  const [documentPath, setDocumentPath] = React.useState("");
+
+  const openDocumentPath = () => {
+    documentPath;
+  };
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>DayPrimer | Home</title>
+      </Head>
+      <div className={classes.root}>
+        <Typography variant="h6" gutterBottom>
+          时间线
+        </Typography>
+        <Button>开始记录行动</Button>
+
+        <Typography variant="h6" gutterBottom>
+          项目
+        </Typography>
+        <Button>添加项目</Button>
+        <Button>编辑项目</Button>
+        <Button>删除项目</Button>
+
+        <Typography variant="h6" gutterBottom>
+          行动
+        </Typography>
+        <Button>添加行动</Button>
+        <Button>编辑行动</Button>
+        <Button>删除行动</Button>
+
+        <Typography variant="h6" gutterBottom>
+          开发人员工具
+        </Typography>
+        <Button variant="contained" color="secondary" onClick={openDocumentPath}>
+          打开数据文件夹
+        </Button>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default HomePage;
