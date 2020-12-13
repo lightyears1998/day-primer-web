3325a3a49ff83f7148bf841ce0854e888fef4bb1
lightyears1998 <lightyears1998@hotmail.com>
2020-12-13 18:00:56 +0800

    chore: 使用用户名区分用户

--- pages/user/sign-in.tsx
+++ pages/user/sign-in.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
@@ -62,10 +61,10 @@ export default function SignInPage(): JSX.Element {
               margin="normal"
               required
               fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
               autoFocus
             />
             <TextField
